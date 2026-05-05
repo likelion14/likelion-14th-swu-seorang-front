00fc5ea8dfb4d43c03a7073975dbfae35824476b
5cool9 <pumpkim12345@naver.com>
2026-05-06 03:01:30 +0900

    init: 프로젝트 초기 세팅

---
+++ src/pages/home/HomeBooth.tsx
@@ -0,0 +1,18 @@
+export default function HomeBooth() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <p style={{ fontFamily: "Moneygraphy", fontSize: "28px" }}>
+        Moneygraphy 폰트 테스트
+      </p>
+
+      <p style={{ fontFamily: "Moneygraphy", fontSize: "18px" }}>
+        가나다라마바사 abcdefg 12345
+      </p>
+
+
+      <p style={{ fontFamily: "system-ui", fontSize: "18px" }}>
+        폰트 테스트용 기본 시스템 폰트
+      </p>
+    </div>
+  );
+}
