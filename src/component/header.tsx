48812623792bea10ebe28270f947a6b9c75c66fb
5cool9 <pumpkim12345@naver.com>
2026-05-06 22:04:21 +0900

    feat: 헤더, 탭바 컴포넌트 추가

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/icon/Lion-Logo.svg";
 import userIcon from "../assets/icon/User-icon.svg";
 import "../component/header.css";
 
 export default function Header() {
-  return (
-    <header className="header">
-      <img src={logo} alt="logo" className="logo" />
-      <img src={userIcon} alt="user" className="user" />
-    </header>
-  );
+    return (
+        <header className="header">
+            <Link to="/">
+                <img src={logo} alt="logo" className="logo" />
+            </Link>
+            <img src={userIcon} alt="user" className="user" />
+        </header>
+    );
 }
